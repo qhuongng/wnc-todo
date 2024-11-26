9c000b91afd0287c49a6a2fb19e5e30567a368d5
qhuongng <nq.huong.2304@gmail.com>
2024-11-26 20:52:00 +0700

    Combine TodoItem with TodoList

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -1,4 +1,5 @@
-import TodoItem from "./TodoItem";
+import { Icon } from "@iconify/react/dist/iconify.js";
+
 import { Todo } from "@/lib/todos";
 
 interface TodoListProps {
@@ -10,7 +11,27 @@ const TodoList: React.FC<TodoListProps> = ({ todos, markTodo }) => {
     return (
         <div className="flex flex-col pb-8 px-8">
             {todos.map((todo: Todo) => (
-                <TodoItem key={todo.id} todo={todo} markTodo={markTodo} />
+                <div
+                    key={todo.id}
+                    className="flex rounded-xl bg-slate-50 shadow-xl mb-4 px-4 items-center justify-between"
+                >
+                    <p
+                        className={`font-semibold text-lg text-wrap ${
+                            todo.completed ? "text-slate-400 line-through" : "text-slate-800"
+                        }`}
+                    >
+                        {todo.content}
+                    </p>
+
+                    {!todo.completed && (
+                        <button
+                            className="btn btn-square btn-primary ml-8"
+                            onClick={() => markTodo(todo.id)}
+                        >
+                            <Icon icon="ic:round-check" style={{ fontSize: "24px" }} />
+                        </button>
+                    )}
+                </div>
             ))}
         </div>
     );
