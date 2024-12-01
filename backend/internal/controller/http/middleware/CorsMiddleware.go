ea5ee898be890face58933cb97aa48333373bef5
hoadang0305 <dangnhathoa22@gmail.com>
2024-12-01 18:16:43 +0700

    cors middleware update

--- backend/internal/controller/http/middleware/CorsMiddleware.go
+++ backend/internal/controller/http/middleware/CorsMiddleware.go
@@ -4,9 +4,10 @@ import "github.com/gin-gonic/gin"
 
 func CorsMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
